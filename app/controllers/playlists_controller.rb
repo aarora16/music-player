4c47b9025c4aa9a2527ff10940bda6284994031b
aarora16 <a.99.anshul@gmail.com>
2020-12-22 13:31:18 -0600

    structure done

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,19 @@
 class PlaylistsController < ApplicationController
+  def index
+    playlists = Playlist.all
+    if playlists
+      render json: playlists
+    else 
+      render json: { message: 'No playlists found' }
+    end
+  end
+
+  def show
+    playlist = Playlist.find_by(id: params[:id])
+    if playlist
+      render json: playlist
+    else
+      render json: { message: 'Playlist not found' }
+    end
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,19 @@
 class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    if songs
+      render json: songs
+    else
+      render json: { message: 'No songs found' }
+    end
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    if song
+      render json: song
+    else 
+      render json: { message: 'Song not found' }
+    end
+  end
 end
